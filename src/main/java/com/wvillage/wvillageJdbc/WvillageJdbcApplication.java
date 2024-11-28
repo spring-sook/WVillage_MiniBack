215d0cdbb0ba19883f207db470214288551a46a5
pjs2024 <jisuk0415@naver.com>
2024-11-28 12:45:44 +0900

    first commit

---
+++ src/main/java/com/wvillage/wvillageJdbc/WvillageJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.wvillage.wvillageJdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvillageJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvillageJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvillage/wvillageJdbc/WvillageJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvillage.wvillageJdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WvillageJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
