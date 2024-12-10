34a1e048b044bb2fed9ba0a25a5e8074610543b7
BeigeSeason <kima606@gmail.com>
2024-12-10 16:28:44 +0900

    24.12.10 강휘 (북마크 여부 True/False)

--- src/main/java/com/wvillage/wvillageJdbc/controller/BookmarkController.java
+++ src/main/java/com/wvillage/wvillageJdbc/controller/BookmarkController.java
@@ -1,4 +1,21 @@
 package com.wvillage.wvillageJdbc.controller;
 
+import com.wvillage.wvillageJdbc.dao.BookmarkDAO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/bookmark")
+@RequiredArgsConstructor
 public class BookmarkController {
+    private BookmarkDAO bookmarkDAO;
+
+    @GetMapping("/isBookmarking")
+    public boolean isBookmarking(@RequestParam String email,
+                                 @RequestParam String postId) {
+        return 1 == bookmarkDAO.isBookmarking(email, postId);
+    }
 }
--- src/main/java/com/wvillage/wvillageJdbc/dao/BookmarkDAO.java
+++ src/main/java/com/wvillage/wvillageJdbc/dao/BookmarkDAO.java
@@ -1,4 +1,25 @@
 package com.wvillage.wvillageJdbc.dao;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
 public class BookmarkDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public int isBookmarking(String email, String postId){
+        String sql = "SELECT COUNT(*) FROM bookmark WHERE BK_EMAIL = ? AND BK_POST = ?";
+
+        try{
+            return jdbcTemplate.queryForObject(sql, new Object[]{email, postId}, Integer.class);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw e;
+        }
+    }
 }
