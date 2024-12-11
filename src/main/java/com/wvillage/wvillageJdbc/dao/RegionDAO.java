81abb123eff258d466ceca72fbaff9bd2a587809
BeigeSeason <kima606@gmail.com>
2024-12-11 14:57:05 +0900

    24.12.10 강휘 (Region 테이블 변경)

--- src/main/java/com/wvillage/wvillageJdbc/dao/RegionDAO.java
+++ src/main/java/com/wvillage/wvillageJdbc/dao/RegionDAO.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RegionDAO {
     private final JdbcTemplate jdbcTemplate;
-    private static final String GET_REGION = "SELECT REGION_SIDO, REGION_SIGUN, REGION_GU, REGION_EMD, REGION_RI " +
+    private static final String GET_REGION = "SELECT REGION_SIDO, REGION_SIGUNGU, REGION_EMD, REGION_RI " +
                                              "FROM REGION WHERE REGION_CODE = ? ";
 
     public List<RegionVO> getRegion(String regionCode) {
@@ -34,13 +34,61 @@ public class RegionDAO {
         public RegionVO mapRow(ResultSet rs, int rowNum) throws SQLException {
             RegionVO region = new RegionVO();
             region.setRegionSido(rs.getString("REGION_SIDO"));
-            region.setRegionSigun(rs.getString("REGION_SIGUN"));
-            region.setRegionGu(rs.getString("REGION_GU"));
+            region.setRegionSiGunGu(rs.getString("REGION_SIGUNGU"));
             region.setRegionEmd(rs.getString("REGION_EMD"));
             region.setRegionRi(rs.getString("REGION_RI"));
             return region;
         }
     }
 
+    // 시도 이름 리스트
+    public List<RegionVO> filterSido(){
+        String sql = """
+                SELECT MIN(REGION_CODE) AS RE_CODE, REGION_SIDO
+                FROM REGION
+                GROUP BY REGION_SIDO
+                """;
+
+        try{
+            return jdbcTemplate.query(sql, new SidoRowMapper());
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    private static class SidoRowMapper implements RowMapper<RegionVO> {
+
+        @Override
+        public RegionVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new RegionVO(
+                    rs.getString("RE_CODE"),
+                    rs.getString("REGION_SIDO")
+            );
+        }
+    }
+
+//    public List<RegionVO> regionFilterList(String regionCode) {
+//        String sql, sido, sigun, gu, emd, ri;
+//        sido = regionCode.substring(0, 2);
+//        sigun = regionCode.substring(2, 4);
+//        gu = regionCode.substring(4, 5);
+//        emd = regionCode.substring(5, 8);
+//        ri = regionCode.substring(8, 11);
+//
+//        if(sigun.equals("00") && !sido.equals("36")) {
+//            sql = "SELECT REGION_CODE, REGION_SIGUN FROM REGION WHERE REGION_CODE = '?%' AND REGION_SIGUN IS NOT NULL";
+//        }else if(sido.equals("36")){
+//            sql = "SELECT REGION_CODE, REGION_EMD FROM REGION WHERE REGION_CODE = '?%'";
+//        }
+//    }
+
+//    private static boolean isRegionEmpty(List<RegionVO> lst) {
+//        for(RegionVO region : lst) {
+//            if(region != null) return false;
+//        }
+//
+//        return true;
+//    }
 
 }
--- src/main/java/com/wvillage/wvillageJdbc/vo/RegionVO.java
+++ src/main/java/com/wvillage/wvillageJdbc/vo/RegionVO.java
@@ -10,8 +10,13 @@ import lombok.*;
 public class RegionVO {
     private String regionCode;
     private String regionSido;
-    private String regionSigun;
-    private String regionGu;
+    private String regionSiGunGu;
     private String regionEmd;
     private String regionRi;
+    private String regionName;
+
+    public RegionVO(String regionCode, String regionName) {
+        this.regionCode = regionCode;
+        this.regionName = regionName;
+    }
 }
